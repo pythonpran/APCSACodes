948c0f3da47d935ee1ca74b6e8a020cd7346b081
Pranav Bangaru <pran2banana@gmail.com>
2021-01-22 09:59:46 -0800

    Made changes to the Uber Driver Class to meet all the requirements for the fields and the methods.

--- src/UberDriver/UberDriver.java
+++ src/UberDriver/UberDriver.java
@@ -1,19 +1,29 @@
 package UberDriver;
 
 public class UberDriver {
-    private final double costPerMile = 0.05;
-
+    private double costPerMile;
+    private int milesPerGallon;
+    private String name;
 
     private double balance;
     private double tankCapacity;
     private double gasLevel;
 
-    private double distance;
-    private double payRate = costPerMile * distance;
     private int passengerNum;
+    private int seats;
+    private double costPerGallon;
 
-
-    public UberDriver(String name, int seats, int payRate, int milesPerGallon, int tankCapacity, int balance, double gasLevel) {
+    public UberDriver(String name, int seats, double costPerMile, int milesPerGallon, int tankCapacity, int balance, int gasLevel, double costPerGallon) {
+        this.costPerMile = costPerMile;
+        this.seats = seats;
+        this.tankCapacity = tankCapacity;
+        this.gasLevel  = gasLevel;
+        if(gasLevel > tankCapacity)
+            this.gasLevel = tankCapacity;
+        this.name = name;
+        this.milesPerGallon = milesPerGallon;
+        this.balance = balance;
+        this.costPerGallon = costPerGallon;
 
     }
 
@@ -21,14 +31,54 @@ public class UberDriver {
 
     }
 
-    public void getGas(){}
+    public double getGas(int distanceFromGasStop){
+        double difference = gasLevel - tankCapacity - distanceFromGasStop * milesPerGallon;
+        if(difference < 0){
+            System.out.println("Not enough gas to reach gas station, find gas station with different distance");
+            return -1;
+        }
+        balance -= costPerGallon * difference;
+        gasLevel = tankCapacity;
+        return gasLevel;
+    }
 
-    public void pickUp(){}
+    public void pickUp(int distanceFromDestination,int numberPicked){
+        if(numberPicked < 0 ){
+            System.out.println("Invalid amount of passengers");
 
-    public void dropOff(){}
+        }if(passengerNum + numberPicked > seats){
+            System.out.println("Not enough space to fit all passengers");
+            passengerNum = seats;
+        }else{
+            passengerNum += numberPicked;
+        }
+
+        gasLevel -= distanceFromDestination / milesPerGallon;
+        balance += costPerMile * distanceFromDestination;
+    }
+
+    public void dropOff(int distanceFromDestination, int numberDropped){
+        if(numberDropped < 0 ){
+            System.out.println("Invalid amount of passengers");
+
+        }if(passengerNum + numberDropped > seats){
+            System.out.println("Not enough space to fit all passengers");
+            passengerNum = seats;
+        }else{
+            passengerNum += numberDropped;
+        }
+
+        gasLevel -= distanceFromDestination / milesPerGallon;
+        balance += costPerMile * distanceFromDestination;
+
+    }
+
+    private void traveling(int distanceFromDestination, int amount){
+
+    }
 
     public String toString(){
-        return String.format("Pay Rate: %d\nPassenger Number: %d",payRate,passengerNum);
+        return String.format("Pay Rate: %d\nPassenger Number: %d",costPerMile,passengerNum);
     }
 
 
--- src/UberDriver/UberDriverClient.java
+++ src/UberDriver/UberDriverClient.java
@@ -2,6 +2,7 @@ package UberDriver;
 
 public class UberDriverClient {
     public static void main(String[]args){
+        UberDriver Carl = new UberDriver("Carl", 5, 0.05, 30, 50,30, 50, 10.0);
 
     }
 }
